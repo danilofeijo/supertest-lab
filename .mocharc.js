0ad4bddfea625eae44f7d5074e569ac5277545f8
Danilo Feijó <danilo.silvafs@gmail.com>
2022-04-27 14:49:13 -0300

    setup: install mocha and supertest

---
+++ .mocharc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // reporter: 'mochawesome',
+  // reporterOptions: 'json=false,reportDir=report,reportFilename=automacao-api',
+  // require: 'test/utils/global.js',
+  spec: ['tests/**/*.test.js']
+}
